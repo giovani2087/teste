5351608ec68cd0aeeb5120f3fba86cda689bff40
giovani2087 <giovani2087@gmail.com>
2021-11-07 23:17:13 -0300

    programinha treinamento

--- typescript/Personagem.ts
+++ typescript/Personagem.ts
@@ -1,33 +1,62 @@
 
-
+import prompt from "prompt-sync";
 class Personagem {
-    nome: string = "";
-    energia: number = 0;
-    vida: number = 0;
-    ataque: number = 0;
-    defesa: number = 0;
+    constructor(public nome: string = "",
+        public energia: number = 0,
+        public vida: number = 0,
+        public ataque: number = 0,
+        public defesa: number = 0) {
+
+    }
+
+}
+
+let teclado = prompt();
+let sansa: Personagem = new Personagem('Sansa Stark', 40, 50, 30, 90);
+let Jon: Personagem = new Personagem('Jon', 70, 60, 50, 30);
+let option: number = 0;
+
+while (option != 9) {
+    console.log(`|********Personagem********|`);
+    console.log(`|1.Escolher Personagem     |`);
+    console.log(`|2.Treinar Ataque          |`);
+    console.log(`|3 Treinar Defesa          |`);
+    console.log(`|4. Imprimir atribuições   |`);
+    console.log(`|9. Sair                   |`);
+    console.log(`|**************************|`);
+
+    option = +teclado(`Escolha uma Ação: `);
+
+    switch (option) {
+        case 1:
+
+            break;
+        case 2:
+            sansa.ataque += 10;
+            break;
+        case 3:
+            sansa.defesa += 10;
+            break;
+        case 4:
+            console.log(`Personagem >>`, { case: 1 });
+            break;
+        case 9:
+            console.log(`Encerrando Treinamento`);
+            break;
+        default:
+            break;
+
+
+
+
+
+    }
+
+
 }
 
 
 
-let sansa: Personagem;
-sansa = new Personagem();
-sansa.nome = 'Sansa Stark';
-sansa.ataque = 10;
-sansa.defesa = 20;
-sansa.vida = 100;
 
-//console.log("person :>>", sansa);
-
-let jon: Personagem = new Personagem();
-jon.nome = "jon Snow";
-jon.ataque = 80;
-jon.defesa = 70;
-jon.energia = 90;
-jon.vida = 40;
-
-//console.log("person :>>", jon);
-
-console.log("person :>>", [jon, sansa]);
 
 
